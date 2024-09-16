e1c62976cd336c7fb1be8f6e1fe7b48c0f2a7e9a
preshitmotghare03 <123preshitmotghare@gmail.com>
2024-09-16 12:46:19 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "preshit@google.com"
+var accountPasswpord = "987483"
+accountCity = "Nagpur"
+let accountState;
+
+//accountId =2
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+accountEmail = "hc@hc.com"
+accountPassword = "123456"
+accountCity = "Bengaluru"
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPasswpord, accountCity, accountState])
