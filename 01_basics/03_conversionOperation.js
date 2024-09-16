38c100ef4cea55e521ab2d74fd9115815297a50f
preshitmotghare03 <123preshitmotghare@gmail.com>
2024-09-16 12:47:07 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,41 @@
+let score = null
+
+//console.log (typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+
+//let stringNumber = String(someNumber)
+//console.log(typeof stringNumber);
+
+//*******************OPerations***************/
+let value = 3
+let negValue = -value
+//console.log(negValue);
+ 
+let str1 = "Hello"
+let str2 = " Preshit"
+
+// console.log(str1 + str2);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+
+
+
+
