59177e65ee38ef92443aac914321d9cf330c73e1
preshitmotghare03 <123preshitmotghare@gmail.com>
2024-09-18 11:37:01 +0000

    01_arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,36 @@
+// array
+
+const myArr = [1, 2, 3, 4, 5]
+const heros = ["Ironman", "Captain America"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+
+//console.log(myArr[0]);
+
+//Array Methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(0)
+//myArr.shift()
+// console.log(myArr.includes(8));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log(typeof newArr);
+
+console.log("A" ,myArr);
+const myn1 = myArr.slice(1,3)
+console.log("B" ,myArr);
+console.log(myn1);
+
+const myn2 = myArr.splice(1,3)
+console.log("C" ,myArr);
+console.log(myn2);
+
+
+
+
