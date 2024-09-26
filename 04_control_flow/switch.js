449b02e02f4afcedcb08514907bc23e779d928c8
preshitmotghare03 <123preshitmotghare@gmail.com>
2024-09-26 12:37:42 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,21 @@
+const month = "feb"
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case "feb":
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("Default value");
+        
+        break;
+}
